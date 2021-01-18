30ea92919ccbcf6628bf2a5de2be631e26e55958
zurriamiang <75233428+zurriamiang@users.noreply.github.com>
2021-01-18 11:44:09 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+
+define( 'APIKEY', 'xxxxxxxxx' ); // harus sama dengan yang disetting di wp-sipd APIKEY SIMDA
+
+define( 'DRIVER', 'xxxxx' ); // driver SQL SERVER (freetds, sql_srv dll)
+
+define( 'HOST_SQL', 'localhost' ); // host untuk database MYSQL
+define( 'DB_SQL', 'SIMDA2021' );
+define( 'USER_SQL', 'SA' );
+define( 'PASS_SQL', '123' );
